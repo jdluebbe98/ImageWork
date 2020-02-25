f56cf18257ba2575ca8b0567c3d66656566d3dd0
luebbejo <luebbejo@LCBLAB2110-13.uccob.uc.edu>
2020-02-25 09:18:38 -0500

    Last commit of class

--- luebbejo_inclass_02_25/Src/Main.py
+++ luebbejo_inclass_02_25/Src/Main.py
@@ -7,6 +7,11 @@ from image_functions import *
 im = Image.open("SiriusAndViolet.jpg")
 print(im.width, im.height, im.mode, im.format) #Displays info about the image
 
-my_image = load_image("SiriusAndViolpet.jpg")
-my_image.show(my_image)
+#my_image = load_image("SiriusAndViolpet.jpg")
+#my_image.show(my_image)
 
+
+#make use of crop_image()
+im = Image.open("SiriusAndViolet.jpg")
+im_cropped = crop_image((200,300,400,500))
+im_cropped.show()
--- luebbejo_inclass_02_25/Src/image_functions.py
+++ luebbejo_inclass_02_25/Src/image_functions.py
@@ -7,6 +7,7 @@ from PIL import Image, ImageFilter, ImageDraw, ImageFont
 import os, sys
 #import requests
 from io import BytesIO
+from Tools.scripts.parseentities import outfile
 
 
 """
@@ -22,5 +23,26 @@ def load_image( filename ) :
         myimage.load()
         return myimage
     except:
-        print("Unable to find image")
+        print("load_image(): Unable to load" + filename)
         return None # None is a keyword that represents a null pointer
+    
+    
+    
+def save_image( imageObject, outfilename ) :
+    """
+    Save an image to disk
+    :param imageObject: The Image to save
+    :param outfilename: The target file
+    """
+    try:
+        imageObject.save( outfilename )
+    except:
+        print("save_image(): Unable to save" + outfilename)
+        
+        
+def crop_image(imageObject, cropRegion): 
+        im_c = imageObject.crop(cropRegion) # (left, top, right, bottom) it's a tuple!
+        return im_c
+
+        
+        
